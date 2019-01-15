02e283b7c6fdfed8dd7599df828b14a202de9ea5
Colin Clark <colinbdclark@gmail.com>
2019-01-15 01:40:13 -0500

    gh-7: Adds keyboard accessibility and alt text to the add button.

--- src/renderer-process/js/add-video-button.js
+++ src/renderer-process/js/add-video-button.js
@@ -10,6 +10,13 @@ https://github.com/colinbdclark/bubbles/raw/master/LICENSE
 fluid.defaults("bubbles.addVideoButton", {
     gradeNames: "fluid.viewComponent",
 
+    invokers: {
+        handleKeyPress: {
+            funcName: "bubbles.addVideoButton.handleKeyPress",
+            args: ["{that}", "{arguments}.0"]
+        }
+    },
+
     events: {
         onAddVideo: null
     },
@@ -20,11 +27,18 @@ fluid.defaults("bubbles.addVideoButton", {
             args: ["{that}"]
         },
 
-        "onCreate.bindOnAddVideo": {
+        "onCreate.bindOnAddVideoClick": {
             priority: "after:renderIcon",
             "this": "{that}.dom.addIcon",
             method: "click",
             args: ["{that}.events.onAddVideo.fire"]
+        },
+
+        "onCreate.bindOnAddVideoKeyPress": {
+            priority: "after:renderIcon",
+            "this": "{that}.dom.addIcon",
+            method: "keypress",
+            args: ["{that}.handleKeyPress"]
         },
 
         "onAddVideo.log": {
@@ -38,16 +52,32 @@ fluid.defaults("bubbles.addVideoButton", {
         addIcon: ".bubbles-add-video-icon"
     },
 
+    strings: {
+        iconAltText: "Add a Video"
+    },
+
     markup: {
-        addIcon: "<svg role=\"button\" tabindex=\"0\" " +
-        "class=\"bubbles-add-video-icon\" " +
-        "xmlns=\"http://www.w3.org/2000/svg\" " +
-        "width=\"12\" height=\"16\" viewBox=\"0 0 12 16\">" +
-        "<path fill-rule=\"evenodd\" " +
-        "d=\"M12 9H7v5H5V9H0V7h5V2h2v5h5v2z\"/></svg>"
+        addIcon: "<svg alt='%iconAltText' role='button' " +
+            "tabindex='0' class='bubbles-add-video-icon' " +
+            "xmlns='http://www.w3.org/2000/svg' " +
+            "width='12' height='16' viewBox='0 0 12 16'>" +
+            "<path fill-rule='evenodd' " +
+            "d='M12 9H7v5H5V9H0V7h5V2h2v5h5v2z'/></svg>"
     }
 });
 
 bubbles.addVideoButton.renderIcon = function (that) {
-    that.container.append(that.options.markup.addIcon);
+    var renderedMarkup = fluid.stringTemplate(
+        that.options.markup.addIcon,
+        that.options.strings
+    );
+
+    that.container.append(renderedMarkup);
+};
+
+bubbles.addVideoButton.handleKeyPress = function (that, evt) {
+    if (evt.key === " " || evt.key === "Enter") {
+        that.events.onAddVideo.fire();
+        return false;
+    }
 };
