1de672f29720cb2c623a2231dc635b7c8c694ef0
Colin Clark <colinbdclark@gmail.com>
2019-01-14 19:57:01 -0500

    gh-7: Adds a collapsible Composition panel on top of stage.

---
+++ src/renderer-process/js/composition-panel.js
@@ -0,0 +1,12 @@
+/*
+Copyright 2019 Colin Clark
+
+Licensed under the MIT license.
+https://github.com/colinbdclark/bubbles/raw/master/LICENSE
+*/
+
+"use strict";
+
+fluid.defaults("bubbles.compositionPanel", {
+    gradeNames: "bubbles.panel"
+});
