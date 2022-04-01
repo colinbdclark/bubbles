25f0ff808fffc7afb96398dab4f81175438909f2
Colin Clark <colinbdclark@gmail.com>
2022-04-01 18:08:10 -0400

    Implments a clamping algorithm for video playback speed.

--- src/renderer-process/js/modulation-matrix-view.js
+++ src/renderer-process/js/modulation-matrix-view.js
@@ -37,9 +37,21 @@ fluid.defaults("bubbles.modulationMatrixView", {
     ],
 
     model: {
-        speed: "{videoLayerView}.videoLayer.player.model.rate",
+        speed: 1.0,
         volume: "{videoLayerView}.video.model.volume"
     },
+
+    modelRelay: [
+        {
+            namespace: "clampSpeedToRatesChromiumCanHandle",
+            target: "{videoLayerView}.videoLayer.player.model.rate",
+            singleTransform: {
+                type: "fluid.transforms.free",
+                func: "bubbles.modulationMatrixView.clampSpeed",
+                args: ["{that}.model.speed"]
+            }
+        }
+    ],
 
     dynamicComponents: {
         modulationView: {
@@ -97,3 +109,17 @@ fluid.defaults("bubbles.modulationMatrixView", {
         container: "<div class='bubbles-modulation-matrix'></div>"
     }
 });
+
+bubbles.modulationMatrixView.clampSpeed = function (speed) {
+    // Chrome seems to have some odd range requirements
+    // for video playback rates these days.
+    if (speed < 0.05) {
+        return 0.0;
+    }
+
+    if (speed >= 0.05 && speed < 0.1) {
+        return 0.1;
+    }
+
+    return speed;
+};
